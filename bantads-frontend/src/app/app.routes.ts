76d7785fe041a73f74f78264ce9117019f23929e
AdrianoZS <94873485+AdrianoZS@users.noreply.github.com>
2025-08-16 21:40:15 -0300

    fix(routes): corrigir caminho de importação do RegisterComponent

--- bantads-frontend/src/app/app.routes.ts
+++ bantads-frontend/src/app/app.routes.ts
@@ -1,5 +1,5 @@
 import { Routes } from '@angular/router';
-import { RegisterComponent } from './features/pages/register/register/register.component';
+import { RegisterComponent } from './features/pages/register/register.component';
 
 export const routes: Routes = [
   {
