e02651319742852e357168957556c5a19005c537
GuilhermeA <guimenegari27@gmail.com>
2025-08-17 19:12:30 -0300

    feat(statement): Added the logic and HTML to the component

--- bantads-frontend/src/app/app.routes.ts
+++ bantads-frontend/src/app/app.routes.ts
@@ -1,10 +1,15 @@
 import { Routes } from '@angular/router';
 import { RegisterComponent } from './features/pages/register/register.component';
+import { StatementComponent } from './features/pages/statement/statement.component';
 
 export const routes: Routes = [
   {
     path: 'cadastro',
     component: RegisterComponent
+  },
+  {
+    path: 'extrato',
+    component: StatementComponent
   },
   { path: '**', redirectTo: '' }
 ];
