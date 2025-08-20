5413190ea25caead534a2f66472e138eefc462ed
ths <thalitasanttos77@gmail.com>
2025-08-20 14:31:53 -0300

    fix: correct application routes

--- bantads-frontend/src/app/features/pages/login/login.component.ts
+++ bantads-frontend/src/app/features/pages/login/login.component.ts
@@ -1,35 +1,49 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
-
+import { Router, RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [CommonModule, ReactiveFormsModule],
+  imports: [CommonModule, ReactiveFormsModule, RouterModule],
   templateUrl: './login.component.html',
   styleUrl: './login.component.css'
 })
-
 export class LoginComponent {
   loginForm: FormGroup;
+  mensagem: string = '';
 
-  constructor(private fb: FormBuilder) {
+  // usuário teste
+  private readonly USER_DATA = {
+    user: 'teste@bantads.com',
+    password: 'teste'
+  };
 
+  constructor(private fb: FormBuilder, private router: Router) {
     this.loginForm = this.fb.group({
-
-      agencia: ['', [Validators.required]],
-      conta: ['', [Validators.required]],
+      user: ['', [Validators.required]],
+      password: ['', [Validators.required]],
       lembrarMe: [false]
     });
   }
-
   onSubmit(): void {
-
     if (this.loginForm.valid) {
-      console.log('Dados do formulário:', this.loginForm.value);
+      const { user, password } = this.loginForm.value;
+      if (user === this.USER_DATA.user && password === this.USER_DATA.password) {
+        this.mensagem = 'Login realizado com sucesso!';
+        // redireciona para /dashboard-cliente
+        this.router.navigate(['/dashboard-cliente']);
+      } else {
+        this.mensagem = 'Usuário ou senha incorretos!';
+      }
     } else {
-      console.log('Formulário inválido. Por favor, preencha todos os campos.');
+      this.mensagem = 'Formulário inválido. Por favor, preencha todos os campos.';
     }
   }
+  abrirCadastro() {
+    this.router.navigate(['/cadastro']);
+  }
+
 }
+
