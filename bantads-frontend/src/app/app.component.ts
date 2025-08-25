884e86f87e4a8330aff634fe563f3bfaa7deed76
thalita <thalitasanttos77@gmail.com>
2025-08-24 21:30:15 -0300

    feat(header): implement header component with login functionality and styling

--- bantads-frontend/src/app/app.component.ts
+++ bantads-frontend/src/app/app.component.ts
@@ -1,13 +1,31 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { HeaderComponent } from './features/components/header/header.component';
+import { NavigationEnd, Router, RouterOutlet } from '@angular/router';
+import { filter } from 'rxjs';
+import { CommonModule } from '@angular/common';
+
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [CommonModule, RouterOutlet, HeaderComponent],
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
   title = 'bantads-frontend';
+  showHeader = false;
+
+  constructor(private router: Router) {
+    this.router.events.pipe(
+      filter((event): event is NavigationEnd => event instanceof NavigationEnd)
+    ).subscribe((event: NavigationEnd) => {
+
+      if (event.url === '/login' || event.url === '/') {
+        this.showHeader = true;
+      } else {
+        this.showHeader = false;
+      }
+    });
+  }
 }
