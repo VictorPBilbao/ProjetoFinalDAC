2a11e847fa37fecf3a21f53df45185becd1557b0
ths <thalitasanttos77@gmail.com>
2025-08-29 11:51:11 -0300

    fix(routes): update import paths for customer feature components

--- bantads-frontend/src/app/app.routes.ts
+++ bantads-frontend/src/app/app.routes.ts
@@ -1,12 +1,12 @@
 import { Routes } from '@angular/router';
 
-import { DepositoComponent } from './features/pages/deposito/deposito.component';
-import { LoginComponent } from './features/pages/login/login.component';
-import { RegisterComponent } from './features/pages/register/register.component';
-import { StatementComponent } from './features/pages/statement/statement.component';
-import { UserDashboardComponent } from './features/pages/user-dashboard/user-dashboard.component';
-import { UserDetailsComponent } from './features/pages/user-details/user-details.component';
-import { ListClientsComponent } from './features/pages/list-clients/list-clients.component';
+import { DepositoComponent } from './features/pages-customer/deposito/deposito.component';
+import { LoginComponent } from './features/pages-customer/login/login.component';
+import { RegisterComponent } from './features/pages-customer/register/register.component';
+import { StatementComponent } from './features/pages-customer/statement/statement.component';
+import { UserDashboardComponent } from './features/pages-customer/user-dashboard/user-dashboard.component';
+import { UserDetailsComponent } from './features/pages-customer/user-details/user-details.component';
+import { ListClientsComponent } from './features/pages-customer/list-clients/list-clients.component';
 
 export const routes: Routes = [
   {
