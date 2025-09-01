b8e0916fe8d775dca01d327aff74f92de31a026c
AdrianoZS <94873485+AdrianoZS@users.noreply.github.com>
2025-08-31 21:00:32 -0300

    feat(manager-details): add manager details component with initial structure feat(manager-list): add manager list component with initial structure style(register): clean up CSS styles and improve input field styles refactor(register): streamline registration form and enhance validation messages

---
+++ bantads-frontend/src/app/features/pages-manager/manager-list/manager-list/manager-list.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-manager-list',
+  imports: [],
+  templateUrl: './manager-list.component.html',
+  styleUrl: './manager-list.component.css'
+})
+export class ManagerListComponent {
+
+}
