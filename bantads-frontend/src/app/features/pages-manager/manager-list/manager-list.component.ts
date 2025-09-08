1962e76412595032624fe94e0a8754a28ce7052a
AdrianoZS <94873485+AdrianoZS@users.noreply.github.com>
2025-09-08 16:53:50 -0300

    feat(manager): add manager list and details components with routing

--- bantads-frontend/src/app/features/pages-manager/manager-list/manager-list.component.ts
+++ bantads-frontend/src/app/features/pages-manager/manager-list/manager-list.component.ts
@@ -1,8 +1,14 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Router, RouterLink } from '@angular/router';
+import { from } from 'rxjs';
 
 @Component({
   selector: 'app-manager-list',
-  imports: [],
+  imports: [
+    CommonModule,
+    RouterLink
+  ],
   templateUrl: './manager-list.component.html',
   styleUrl: './manager-list.component.css'
 })
