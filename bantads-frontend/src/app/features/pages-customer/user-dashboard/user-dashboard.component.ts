275f421d1445f580f21992f47d06d835259a5276
vkataguiri <viniciuskataguiri@gmail.com>
2025-09-22 18:24:47 -0300

    feat(user-dashboard): add links to other pages, make balance view dynamic

--- bantads-frontend/src/app/features/pages-customer/user-dashboard/user-dashboard.component.ts
+++ bantads-frontend/src/app/features/pages-customer/user-dashboard/user-dashboard.component.ts
@@ -1,11 +1,23 @@
+import { DecimalPipe, NgStyle } from '@angular/common';
 import { Component } from '@angular/core';
 
-import { StatementItemComponent } from '../../components/ui/statement-item/statement-item.component';
+import { Cliente } from '../../models/cliente.model';
+import { AuthService } from '../../services/auth/auth.service';
 
 @Component({
-  selector: 'app-user-dashboard',
-  imports: [StatementItemComponent],
-  templateUrl: './user-dashboard.component.html',
-  styleUrl: './user-dashboard.component.css',
+    selector: 'app-user-dashboard',
+    imports: [NgStyle, DecimalPipe],
+    templateUrl: './user-dashboard.component.html',
+    styleUrl: './user-dashboard.component.css',
 })
-export class UserDashboardComponent {}
+export class UserDashboardComponent {
+    user!: Cliente;
+    balance!: number;
+    // monthlyDeposit!: number;
+
+    constructor(private authService: AuthService) {
+        this.user = authService.getUser();
+        this.balance = this.user.saldo ? this.user.saldo : 0;
+        console.log(this.user);
+    }
+}
