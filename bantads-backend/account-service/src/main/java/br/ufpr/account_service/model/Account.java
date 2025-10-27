ddfa06ec5996c2e608087fa557c82031a51216ee
GuilhermeA <guimenegari27@gmail.com>
2025-10-27 12:32:47 -0300

    feat: created all of the models needed for account

--- bantads-backend/account-service/src/main/java/br/ufpr/account_service/model/Account.java
+++ bantads-backend/account-service/src/main/java/br/ufpr/account_service/model/Account.java
@@ -1,5 +1,44 @@
 package br.ufpr.account_service.model;
 
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import br.ufpr.account_service.model.Transaction;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+import java.util.List;
+
+@Entity
+@Table(name = "account", schema = "account_schema")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class Account {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "client_id", nullable = false, length = 11)
+    private String clientId;
+
+    @Column(name = "account_number", nullable = false, length = 20)
+    private String accountNumber;
+
+    @Column(name = "creation_date", nullable = false)
+    private LocalDateTime creationDate;
+
+    @Column(name = "balance", nullable = false)
+    private BigDecimal balance;
+
+    @Column(name = "limit", nullable = false)
+    private BigDecimal limit;
+
+    @Column(name = "manager", nullable = false, length = 100)
+    private String manager;
+
+    @OneToMany(mappedBy = "account", cascade = CascadeType.ALL, fetch = FetchType.LAZY)
+    private List<Transaction> transactions;
 
 }
