14814470eb421cdd430267c63470ac8838ef6326
ths <thalitasanttos77@gmail.com>
2025-11-19 10:33:24 -0300

    refactor: reorganize imports and update account model properties for clarity

--- bantads-backend/account-service/src/main/java/br/ufpr/account_service/model/Record.java
+++ bantads-backend/account-service/src/main/java/br/ufpr/account_service/model/Record.java
@@ -1,17 +1,18 @@
 package br.ufpr.account_service.model;
 
-import jakarta.persistence.*;
 import java.math.BigDecimal;
 import java.util.Date;
 import java.util.List;
-import jakarta.persistence.*;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import jakarta.persistence.Transient;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-
-import java.math.BigDecimal;
-import java.time.LocalDateTime;
-import br.ufpr.account_service.model.Transaction;
 
 @Entity
 @Table(name = "record", schema = "account_schema")
@@ -19,10 +20,15 @@ import br.ufpr.account_service.model.Transaction;
 @NoArgsConstructor
 @AllArgsConstructor
 public class Record {
+
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
     private Date date;
+
     private BigDecimal consolidatedBalance;
+
+    @Transient
     private List<Transaction> transactions;
 }
